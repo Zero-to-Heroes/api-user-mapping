56d0ef9f5ad0cb36f19025c6b30b81d731fb6044
sebastientromp <sebastien.tromp@gmail.com>
2021-04-30 10:38:48 +0200

    remove verbose logs

--- src/process-save-mapping.ts
+++ src/process-save-mapping.ts
@@ -5,14 +5,12 @@ import { getConnection } from './db/rds';
 import { Input } from './sqs-event';
 
 export default async (event, context): Promise<any> => {
-	console.log('received event', event);
 	const events: readonly Input[] = (event.Records as any[])
 		.map(event => JSON.parse(event.body))
 		.reduce((a, b) => a.concat(b), []);
 
 	const mysql = await getConnection();
 	for (const ev of events) {
-		console.log('processing event', ev);
 		await processEvent(ev, mysql);
 	}
 	await mysql.end();
@@ -22,7 +20,6 @@ export default async (event, context): Promise<any> => {
 		isBase64Encoded: false,
 		body: null,
 	};
-	console.log('sending back success reponse');
 	return response;
 };
 
@@ -32,6 +29,5 @@ const processEvent = async (input: Input, mysql: ServerlessMysql) => {
 		INSERT IGNORE INTO user_mapping (userId, userName)
 		VALUES (${escape(input.userId)}, ${escape(input.userName)})
 	`;
-	console.log('prepared query', query);
 	await mysql.query(query);
 };
